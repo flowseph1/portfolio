c2d5a9c03c4a842f7e8597869c53a54bd1bc53b2
flowseph1 <josephthenewandarmonius@gmail.com>
2023-12-28 20:58:58 -0600

    feat: update logo and metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,12 +13,14 @@ export const metadata: Metadata = {
     "Dedicated Front End Developer with extensive expertise in React.js and React Native development. Solid Knowledge of JavaScript and TypeScript, with a passion for simple, maintainable, and readable code. Experienced in optimizing responsive designs for diverse platforms. Familiar with CI/CD pipelines, Git version control, and a proven track record of creating intuitive user interfaces tailored for optimal performance.",
   openGraph: {
     title: "Jose Acosta",
-    description: "Front End Developer | React.js | React Native | Next.js",
+    description:
+      "Dedicated Front End Developer with extensive expertise in React.js and React Native development. Solid Knowledge of JavaScript and TypeScript, with a passion for simple, maintainable, and readable code. ",
+
     images: [
       {
         url: "/portfolio-logo.png",
-        width: 600,
-        height: 600,
+        width: 631,
+        height: 663,
         alt: "Jose Acosta Portfolio Logo",
       },
     ],
