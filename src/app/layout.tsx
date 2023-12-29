bebbebd071c6fd0c58bd8b053c34b7784d0e9759
flowseph1 <josephthenewandarmonius@gmail.com>
2023-12-28 21:04:25 -0600

    feat: update twitter metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,25 @@ export const metadata: Metadata = {
     "Dedicated Front End Developer with extensive expertise in React.js and React Native development. Solid Knowledge of JavaScript and TypeScript, with a passion for simple, maintainable, and readable code. Experienced in optimizing responsive designs for diverse platforms. Familiar with CI/CD pipelines, Git version control, and a proven track record of creating intuitive user interfaces tailored for optimal performance.",
   openGraph: {
     title: "Jose Acosta",
+    url: "https://www.joseacosta.dev/",
     description:
       "Dedicated Front End Developer with extensive expertise in React.js and React Native development. Solid Knowledge of JavaScript and TypeScript, with a passion for simple, maintainable, and readable code. ",
 
+    images: [
+      {
+        url: "/portfolio-logo.png",
+        width: 631,
+        height: 663,
+        alt: "Jose Acosta Portfolio Logo",
+      },
+    ],
+  },
+
+  twitter: {
+    title: "Jose Acosta",
+    description:
+      "Dedicated Front End Developer with extensive expertise in React.js and React Native development. Solid Knowledge of JavaScript and TypeScript, with a passion for simple, maintainable, and readable code. ",
+    card: "summary_large_image",
     images: [
       {
         url: "/portfolio-logo.png",
